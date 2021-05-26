67a689953de2b98763aa25b82e7c9b0b9eadb856
sunfl0w <lucaanthonyschwarz@googlemail.com>
2021-05-26 13:42:40 +0200

    Added a memory dump and clear only operation. Fixed some timing issues

--- MemProgrammer.ino
+++ MemProgrammer.ino
@@ -10,8 +10,15 @@ const int timeoutTime = 3000;  //Timeout time in ms
 int currentAddress = 0;
 int timeSinceLastData = 0;
 
+enum OperatingMode {
+    InvalidMode,
+    Program,
+    MemoryDump,
+    Erase
+};
+
 enum StorageDevice {
-	Invalid,
+	InvalidDevice,
 	AT28CSeries,
 	SST39SFSeries
 };
@@ -29,6 +36,7 @@ void setMemoryAddress(int address, bool enableChipOutput) {
 	digitalWrite(latchPin, LOW);
 	digitalWrite(latchPin, HIGH);
 	digitalWrite(latchPin, LOW);
+    delay(1);
 }
 
 void setOutputData(byte data) {
@@ -44,9 +52,9 @@ void writeToMemory(StorageDevice storageDevice, int address, byte data) {
 		setIOPinsMode(OUTPUT);
 		setOutputData(data);
 		digitalWrite(writeEnablePin, LOW);
-		delayMicroseconds(1);
-		digitalWrite(writeEnablePin, HIGH);
 		delayMicroseconds(10);
+		digitalWrite(writeEnablePin, HIGH);
+        delay(10);
 	} else if (storageDevice == StorageDevice::SST39SFSeries) {
 		digitalWrite(writeEnablePin, HIGH);
 		setIOPinsMode(OUTPUT);
@@ -99,7 +107,7 @@ byte readFromMemory(int address) {
 void clearMemory(StorageDevice storageDevice, int maxAddress) {
 	if (storageDevice == StorageDevice::AT28CSeries) {
 		for (int address = 0; address < maxAddress; address++) {
-			writeToMemory(storageDevice, address, 0x00);
+			writeToMemory(storageDevice, address, 0);
 		}
 	} else if (storageDevice == StorageDevice::SST39SFSeries) {
 		digitalWrite(writeEnablePin, HIGH);
@@ -162,6 +170,7 @@ void printMemoryData(int maxAddress) {
 		byte data[16];
 		for (int offset = 0; offset <= 15; offset += 1) {
 			data[offset] = readFromMemory(base + offset);
+            delayMicroseconds(10);
 		}
 
 		char buf[100];
@@ -173,13 +182,34 @@ void printMemoryData(int maxAddress) {
 	}
 }
 
-StorageDevice getStorageDeviceToProgram() {
+OperatingMode getOperatingMode() {
+    Serial.println("Send operating mode");
+
+    delay(1000);
+
+    String operatingModeString = Serial.readString();
+
+    if(operatingModeString == "Program") {
+        Serial.println("Operating mode " + operatingModeString + " set");
+        return OperatingMode::Program;
+    } else if(operatingModeString == "MemoryDump") {
+        Serial.println("Operating mode " + operatingModeString + " set");
+        return OperatingMode::MemoryDump;
+    } else if(operatingModeString == "Erase") {
+        Serial.println("Operating mode " + operatingModeString + " set");
+        return OperatingMode::Erase;
+    } else {
+        Serial.println("Operating mode " + operatingModeString + " not recognized");
+        return OperatingMode::InvalidMode;
+    }
+}
+
+StorageDevice getStorageDevice() {
 	Serial.println("Send storage device type");
 
 	delay(1000);
 
 	String deviceType = Serial.readString();
-	StorageDevice deviceTypeEnum = StorageDevice::Invalid;
 
 	if (deviceType == "AT28CSeries") {
 		Serial.println("Device type " + deviceType + " set");;
@@ -189,11 +219,11 @@ StorageDevice getStorageDeviceToProgram() {
 		return StorageDevice::SST39SFSeries;
 	} else {
 		Serial.println("Device type " + deviceType + " not recognized");
-		return StorageDevice::Invalid;
+		return StorageDevice::InvalidDevice;
 	}
 }
 
-int getDataSizeToProgram() {
+int getDataSize() {
 	Serial.println("Send data size in bytes");
 
 	delay(1000);
@@ -202,12 +232,17 @@ int getDataSizeToProgram() {
 
 	if (numberOfBytes <= 0) {
 		Serial.println("Data size " + String(numberOfBytes) + " invalid");
-		return 0;
+		return -1;
 	} else {
 		Serial.println("Data size is " + String(numberOfBytes) + " bytes");
 		return numberOfBytes;
 	}
 }
+
+void memoryDump(int numBytes) {
+    Serial.println("Memory dump");
+	printMemoryData(numBytes);
+} 
 
 void setup() {
 	Serial.begin(57600);
@@ -218,43 +253,77 @@ void setup() {
 	digitalWrite(writeEnablePin, HIGH);
 	pinMode(writeEnablePin, OUTPUT);
 
-	StorageDevice storageDevice = getStorageDeviceToProgram();
+    OperatingMode operatingMode = getOperatingMode();
+    if(operatingMode == OperatingMode::InvalidMode) {
+        return;
+    }
+
+    delay(1000);
+
+	StorageDevice storageDevice = getStorageDevice();
+    if(storageDevice == StorageDevice::InvalidDevice) {
+        return;
+    }
+
 	delay(1000);
-	int bytesToProgram = getDataSizeToProgram();
+
+	int numBytes = getDataSize();
+    if(numBytes == -1) {
+        return;
+    }
+
 	delay(1000);
 
-	Serial.println("Clearing memory");
-	clearMemory(storageDevice, bytesToProgram);
-	Serial.println("Memory cleared");
-	delay(1000);
+    if(operatingMode == OperatingMode::Erase) {
+        Serial.println("Clearing memory");
+        clearMemory(storageDevice, numBytes);
+        Serial.println("Memory cleared");
 
-	Serial.println("Commencing programming");
-	Serial.println("Send next byte");
+        delay(1000);
 
-	while (timeSinceLastData < timeoutTime) {
-		int now = millis();
-		if (Serial.available() > 0 && currentAddress < bytesToProgram) {
-			byte data = Serial.read();
+        memoryDump(numBytes);
+        Serial.println("Goodbye");
+	    Serial.println("Have a nice day :D");
+        return;
+    } else if(operatingMode == OperatingMode::MemoryDump) {
+        memoryDump(numBytes);
+        Serial.println("Goodbye");
+	    Serial.println("Have a nice day :D");
+        return;
+    } else if(operatingMode == OperatingMode::Program) {
+        Serial.println("Clearing memory");
+        clearMemory(storageDevice, numBytes);
+        Serial.println("Memory cleared");
+        delay(1000);
 
-			//Use for debugging
-			Serial.print("Received:");
-			Serial.println(data, DEC);
+        Serial.println("Commencing programming");
+        Serial.println("Send next byte");
 
-			writeToMemory(storageDevice, currentAddress, data);
-			currentAddress++;
-			timeSinceLastData = 0;
-			Serial.println("Send next byte");
-		} else {
-			timeSinceLastData += millis() - now;
-		}
-	}
+        while (timeSinceLastData < timeoutTime) {
+            int now = millis();
+            if (Serial.available() > 0 && currentAddress < numBytes) {
+                byte data = Serial.read();
 
-	Serial.println("No more data detected");
-	Serial.println("Memory programmed");
-	Serial.println("Memory dump");
-	printMemoryData(bytesToProgram);
-	Serial.println("Goodbye");
-	Serial.println("Have a nice day :D");
+                //Use for debugging
+                Serial.print("Received:");
+                Serial.println(data, DEC);
+
+                writeToMemory(storageDevice, currentAddress, data);
+                currentAddress++;
+                timeSinceLastData = 0;
+                Serial.println("Send next byte");
+            } else {
+                timeSinceLastData += millis() - now;
+            }
+        }
+
+        Serial.println("No more data detected");
+        Serial.println("Memory programmed");
+        memoryDump(numBytes);
+        Serial.println("Goodbye");
+        Serial.println("Have a nice day :D");
+    }
+    return;
 }
 
 void loop() {}
--- MemProgrammer.py
+++ MemProgrammer.py
@@ -4,67 +4,112 @@ import time
 
 def main():
     programmingEnded = False
+    operationEnded = False
     dataRequested = False
     currentIndex = 0
 
     # Parsing command line arguments
     argumentParser = argparse.ArgumentParser()
     argumentParser.add_argument("-r", "--baudRate", required=True)
-    argumentParser.add_argument("-d", "--dataFile", required=True)
+    argumentParser.add_argument("-d", "--dataFile")
     argumentParser.add_argument("-p", "--serialPort", required=True)
     argumentParser.add_argument("-s", "--storageDevice", required=True)
+    argumentParser.add_argument("-m", "--operatingMode", required=True)
     args = argumentParser.parse_args()
 
     baudRate = args.baudRate
     dataFile = args.dataFile
     serialPort = args.serialPort
     deviceType = args.storageDevice
+    operatingMode = args.operatingMode
 
     print("Baudrate: " + baudRate)
-    print("Data file: " +  dataFile)
+    if dataFile is not None:
+        print("Data file: " +  dataFile)
     print("Serial port: " + serialPort)
     print("Storage device: " + deviceType)
+    print("Operating mode: " + operatingMode)
 
-    print("Reading binary data file")
+    if operatingMode == "Program":
+        if dataFile == None:
+            print("Data file not set")
+            return
 
-    data = []
-    with open(dataFile, "rb") as file:
-        data = file.read()
+        print("Reading binary data file")
 
-    print("Opening serial port")
+        data = []
+        with open(dataFile, "rb") as file:
+            data = file.read()
 
-    ser = serial.Serial(serialPort, baudRate, serial.EIGHTBITS);
-    ser.flush()
-    time.sleep(3)
+        print("Opening serial port")
 
-    while(not programmingEnded):
-        line = ser.readline().decode('utf-8')
-        line = line.replace('\r', '')
-        line = line.replace('\n', '')
-        print("Arduino: " + line)
+        ser = serial.Serial(serialPort, baudRate, serial.EIGHTBITS);
+        ser.flush()
+        time.sleep(3)
 
-        if(line == "Send storage device type"):
-            print("Sending storage device type")
-            ser.write(deviceType.encode())
+        while not programmingEnded:
+            line = ser.readline().decode('utf-8')
+            line = line.replace('\r', '')
+            line = line.replace('\n', '')
+            print("Arduino: " + line)
 
-        if(line == "Send data size in bytes"):
-            print("Sending data size in bytes")
-            print("Size is " + str(len(data)) + " bytes")
-            ser.write(str(len(data)).encode())
+            if line == "Send storage device type":
+                print("Sending storage device type")
+                ser.write(deviceType.encode())
 
-        if(line == "Send next byte" and currentIndex < len(data)):
-            # Use for debugging
-            # print("Sending: " + str(data[currentIndex]))
+            if line == "Send data size in bytes":
+                print("Sending data size in bytes")
+                print("Size is " + str(len(data)) + " bytes")
+                ser.write(str(len(data)).encode())
 
-            ser.write(bytes([data[currentIndex]]))
-            currentIndex = currentIndex + 1
-            time.sleep(0.001) # sleep 1 ms to move data more slowly
+            if line == "Send operating mode":
+                print("Sending operating mode")
+                ser.write(operatingMode.encode())
 
-        if(line == "Goodbye"):
-            print("Closing connection")
-            programmingEnded = True
+            if line == "Send next byte" and currentIndex < len(data):
+                # Use for debugging
+                # print("Sending: " + str(data[currentIndex]))
 
-    ser.close();
+                ser.write(bytes([data[currentIndex]]))
+                currentIndex = currentIndex + 1
+                time.sleep(0.001) # sleep 1 ms to move data more slowly
+
+            if line == "Goodbye":
+                print("Closing connection")
+                programmingEnded = True
+
+        ser.close();
+    elif operatingMode == "MemoryDump" or operatingMode == "Erase":
+        print("Opening serial port")
+
+        ser = serial.Serial(serialPort, baudRate, serial.EIGHTBITS);
+        ser.flush()
+        time.sleep(3)
+
+        while not operationEnded:
+            line = ser.readline().decode('utf-8')
+            line = line.replace('\r', '')
+            line = line.replace('\n', '')
+            print("Arduino: " + line)
+
+            if line == "Send storage device type":
+                print("Sending storage device type")
+                ser.write(deviceType.encode())
+
+            if line == "Send data size in bytes":
+                print("Sending data size in bytes")
+                print("Size is " + str(8192) + " bytes")
+                ser.write(str(8192).encode())
+            
+            if line == "Send operating mode":
+                print("Sending operating mode")
+                ser.write(operatingMode.encode())
+
+            if line == "Goodbye":
+                print("Closing connection")
+                operationEnded = True
+
+        ser.close();
 
 if __name__ == "__main__":
     main()
